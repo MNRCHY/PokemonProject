03270de65d8ae1e0d8d4e0fd9bade1ffd9a9e41e
Moechtar Wira <moechtarwira252@gmail.com>
2023-02-15 17:46:11 +0700

    Add Pokemon Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div className='bg-Main h-14 flex flex-col justify-center'>
-        <h1 className='text-white opacity-50'> Created by <a className='hover:text-black' href='https://www.linkedin.com/in/moechtarwira/' target='_blank' rel='noreferrer noopener'>Moechtar Wira</a> </h1>
+    <div className='bg-Main flex flex-col gap-5 justify-center py-5'>
+      
+      <div className='text-white font-semibold flex gap-4 justify-center'>
+        <Link className='hover:text-gray-300' to='/'> Home </Link>
+        <Link className='hover:text-gray-300' to='/pokemon'> Pokemon </Link>
+        <Link className='hover:text-gray-300' to='/item'> Items </Link>
+        <Link className='hover:text-gray-300' to='/berry'> Berries </Link>
+      </div>
+      
+      <h5 className='text-white opacity-50'>
+        Created by <a className='hover:text-gray-300' href='https://www.linkedin.com/in/moechtarwira/' target='_blank' rel='noreferrer noopener'>
+          Moechtar Wira
+        </a>
+      </h5>
     </div>
   )
 }
--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Logo from '../assets/Logo.webp'
+import Footer from '../components/Footer'
+import { getAllPokemons } from '../redux/actions/pokeActions'
 
 function Pokemon() {
+
+  const dispatch = useDispatch()
+  const { pokemons } = useSelector((state) => state.pokemon)
+
+  useEffect(() => {
+    dispatch(getAllPokemons())
+  }, [dispatch])
+
   return (
-    <div>This is pokemon page</div>
+    <div className='bg-Main'>
+
+      <header className='flex justify-center p-10'>
+        <img className='w-96' src={Logo} alt='Logo'/>
+      </header>
+
+      <div className='bg-white grid grid-cols-5 gap-3 mx-20 p-5 rounded-md'>
+        {
+          pokemons.map((pokemon, index) => (
+            <div className='bg-green-500 p-2 rounded-sm' key={index}>
+              <h1 className='text-white font-semibold'>{pokemon.name}</h1>
+            </div>
+          ))
+        }
+      </div>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
